929d6fb6721c71af9c3f931362c7b1fb8e230d5c
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-27 14:19:59 -0400

    first commit

---
+++ database.rb
@@ -0,0 +1,13 @@
+class minions
+  attr_accessor :name :phone :number :address :position :salary :slack :github
+
+  def initialize
+    self.name
+    self.phone
+    self.number
+    self.address
+    self.position
+    self.salary
+    self.slack
+    self.github
+  end
