bb9d0daea15d838e59dea7eaa7bccca4a1f8359a
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-07-27 22:00:41 -0400

    not quite good enugh

--- database.rb
+++ database.rb
@@ -1,74 +1,97 @@
-names = ["Gavin", "Jason", "Toni"]
-
 class Peeps
   attr_accessor :name, :phone, :address, :position , :salary, :slack, :github
 
   def initialize(name, phone, address, position, salary, slack, github)
-    self.name = name
-    self.phone = phone
-    self.address = address
-    self.position = position
-    self.salary = salary
-    self.slack = slack
-    self.github = github
   end
-end
-
-class Add_person
-person = Peeps.new
-person << %w([name] phone address position salary slack github)
-end
-
-
-class Search_person
-end
-
-class Delete_person
 end
 
 class Menu
+  def initialize
+    @peeps = []
+end
+
+def prompt
+  puts "Please Select the corresponding letter (A, S or D) as follows:
+    {
+      A: Add a person
+      S: Search for a person
+      D: Delete a person
+    }"
+  chosen = gets.chomp
+  if chosen == "D"
+    break
+  end
+end
+
+# case chosen
+# when A
+#   add_person
+# when S
+#   search_person
+# when D
+#   delete_person
+# when C
+#   cancel_search
+# else
+#   puts "Please only select A, S, or D"
+# end
+
   def prompt
-    puts "Please Select the corresponding letter (A, S or D) as follows:
-      {
-        A: Add a person
-        S: Search for a person
-        D: Delete a person
-      }"
-    selected = gets.chomp
+    loop do
+    puts "A: Add a person"
+    puts "S: Search for a person"
+    puts "D: Delete a person"
+    puts "C: Cancel"
+    picked = gets.chomp
+
+    if picked == "A"
+      puts "Please provide first and last name"
+      Add_person
+    elsif picked == "S"
+      puts "Please provide first and last name"
+      Search_person
+    elsif picked == "D"
+      puts "Please provide first and last name"
+      Delete_person
+    elsif picked == "C"
+    end
   end
 
-case selected
-when A
+def method_name
   add_person
-when S
-  search_person
-when D
-  delete_person
-else
-  puts "Please only select A, S, or D"
+person = Peeps.new
+@peeps << person
+person << %w([name] phone address position salary slack github)
+end
+
+def search_person
+  puts "Who are you looking for "
+  search_person = gets.chomp!
+
+  for person in @peeps
+  if person.name == search_person
+    puts "That is:
+          #{person.name}
+          #{person.phone}
+          #{person.address}
+          #{person.position}
+          #{person.salary}
+          #{person.slack}
+          #{person.github}"
+    return
+  else
+    puts "Unable to find #{search_person}, they maybe M.I.A."
+  end
+end
+
+def delete_person
+  delete_person = gets.chomp
+end
+
+def cancel_search
+  puts "Thank you and have a great day"
+  exit
 end
 
 menu = Menu.new
 menu.prompt
-
-
-
-  # def  selected(name)
-  # add_person
-  #   case sound
-  #   when "woof"
-  #     file.puts "I'm a good dog"
-  #   when /.*e.*/
-  #     file.puts "I have an e in my sound"
-  #   else
-  #     file.puts "I make no sound"
-  #   end
-  # end
-
-# first_name = "Jane"
-# middle_name = ""
-# last_name = "Smith"
-# name = first_name
-# name += " #{middle_name}" if middle_name.length > 0
-# name += " #{last_name}"
-# name # => "Jane Smith"
